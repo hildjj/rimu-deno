70224c68ab2939b2afdc208c5816afa361fe4b17
Stuart Rackham <srackham@gmail.com>
2020-02-26 14:04:47 +1300

    docs: Update minimal example.

--- src/minimal-example.ts
+++ src/minimal-example.ts
@@ -2,7 +2,8 @@
  Simplest Rimu TypeScript application.
  */
 
-// import * as rimu from "https://raw.github.com/srackham/rimu-deno/master/src/rimu.ts";
-import * as rimu from "../mod.ts";
+// import * as rimu from "https://raw.github.com/srackham/rimu-deno/master/mod.ts"
+// import * as rimu from "https://raw.github.com/srackham/rimu-deno/v0.2.0/mod.ts"
+import * as rimu from "../mod.ts"
 
 console.log(rimu.render("Hello *Rimu*!"));
