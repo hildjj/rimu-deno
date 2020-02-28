a4c4c186d8cc6433a5e1a60381abc64719ac59c0
Stuart Rackham <srackham@gmail.com>
2020-02-29 11:40:03 +1300

    style: Code formatting.

--- src/minimal-example.ts
+++ src/minimal-example.ts
@@ -4,6 +4,6 @@
 
 // import * as rimu from "https://raw.github.com/srackham/rimu-deno/master/mod.ts"
 // import * as rimu from "https://raw.github.com/srackham/rimu-deno/v0.2.0/mod.ts"
-import * as rimu from "../mod.ts"
+import * as rimu from "../mod.ts";
 
 console.log(rimu.render("Hello *Rimu*!"));
